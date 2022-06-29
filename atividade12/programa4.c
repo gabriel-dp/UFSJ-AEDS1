3d7c3fa9d44c7980cc635bfaa5cab51451422d9c
gabrieldp23 <gabriel.meira.2004@gmail.com>
2022-06-28 21:10:04 -0300

    fix program4

--- atividade12/programa4.c
+++ atividade12/programa4.c
@@ -3,41 +3,48 @@
 
 #define LIMITE 50
 
-int indiceChar (char caractere, char string[LIMITE]) {
+void removeEnter (char str[]) {
+    if (str[strlen(str)-1] == '\n') str[strlen(str)-1] = '\0';
+}
+
+int indiceChar (char caractere, char string[]) {
     for (int i = 0; i < strlen(string); i++) {
         if (string[i] == caractere) return i;
     }
     return -1;
 }
 
-int stringContem (char substring[LIMITE], char string[LIMITE]) {
-    int substringlen = strlen(substring);
-
-    int contem = 1;
+int stringContem (char substring[], char string[]) {
+    int contem;
     do {
-        int primeiroIndice = indiceChar(substring[0], string);
-
-        if (primeiroIndice == -1) return 0;
-        for (int i = primeiroIndice; i < primeiroIndice+substringlen; i++) {
+        contem = 1;
+        if (indiceChar(substring[0], string) == -1) return 0;
+        for (int i = 0; i < strlen(substring); i++) {
             if (indiceChar(substring[i], string) == -1) {
-                string += i;
+                string += (i+1);
                 contem = 0;
             }
         }
     } while (contem == 0);
-    
     return 1;
 }
 
 void main () {
     char string[LIMITE], substring[LIMITE];
+
     printf("\nInsira a string: ");
     fgets(string, LIMITE, stdin);
     setbuf(stdin, NULL);
+    removeEnter(string);
 
-    printf("\nInsira a substring: ");
+    printf("Insira a substring: ");
     fgets(substring, LIMITE, stdin);
     setbuf(stdin, NULL);
+    removeEnter(substring);
 
-    printf("\n%d\n\n", stringContem(substring, string));
+    if (stringContem(substring, string)) {
+        printf("\nA substring '%s' esta contida na string '%s'\n\n", substring, string);
+    } else {
+        printf("\nA substring '%s' nao esta contida na string '%s'\n\n", substring, string);
+    }
 }
